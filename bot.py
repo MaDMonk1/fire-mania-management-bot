9bfe18d6bbc069eba095421a29a54b1c262ba9a3
MaDMonk1 <37941989+MaDMonk1@users.noreply.github.com>
2018-07-10 00:36:33 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,14 +18,21 @@ async def on_member_join(member):
   role = discord.utils.get(member.server.roles, name="Member")
   await client.add_roles(member, role)
   emb = (discord.Embed(description=None, colour=0x3DF270))
-  welcome = client.get_channel("443483341354696724")
-  emb.add_field(name="New Member", value="Welcome to SASRP, <@%s>! Have a great time with your RP. Before going anywhere, feel free to check out the useful links text channel! If you need staff, feel free to use @staff and staff will be with you as soon as possible! Also say '?INFORM' to get started! " % (member.id), inline=False)
+  welcome = client.get_channel("465682599008927754")
+  emb.add_field(name="New Member", value="Welcome to Adrenaline Gaming, <@%s>! Have a great time here. If you need staff, feel free to use @staff and staff will be with you as soon as possible! " % (member.id), inline=False)
+  await client.send_message(welcome, embed=emb)
+
+@client.event
+async def on_member_leave(member):
+  emb = (discord.Embed(description=None, colour=0x3DF270))
+  welcome = client.get_channel("465682599008927754")
+  emb.add_field(name="Member Left", value="<@%s> left the server :frowning: " % (member.id), inline=False)
   await client.send_message(welcome, embed=emb)
 
 @client.event
 async def on_ready():
   print("Management || Bot is Online and ready.")
-  await client.change_presence(game=discord.Game(name="SASRP BOT | ?cmds"))
+  await client.change_presence(game=discord.Game(name="AG BOT | ?cmds"))
 
 
 @client.event
@@ -41,38 +48,36 @@ async def on_message(message):
                     return
     if message.content.upper().startswith('?TICKET'):
                                                     args = message.content.split(" ")
-                                                    chan = client.get_channel("443485820721299457")
+                                                    chan = client.get_channel("466022799463415828")
                                                     emb = (discord.Embed(description=None, colour=0x3DF270))
-                                                    emb.add_field(name="Bug Report Ticket by %s" % (message.author), value="%s" % (" ".join(args[1:])), inline=False)
+                                                    emb.add_field(name="Bug/Support/Report Ticket by %s" % (message.author), value="%s" % (" ".join(args[1:])), inline=False)
                                                     emb2 = (discord.Embed(description=None, colour=0x3DF270))
-                                                    emb2.add_field(name="Success!", value="I successfully created a bug report ticket! You have reported the following: %s" % (" ".join(args[1:])), inline=False)
+                                                    emb2.add_field(name="Success!", value="I successfully created a ticket! You have reported the following: %s" % (" ".join(args[1:])), inline=False)
                                                     await client.send_message(message.channel, embed=emb2)
                                                     await client.send_message(chan, embed=emb)
     if message.content.upper().startswith('?INFORM'):
-                                                    embed = discord.Embed(name="SASRP's info".format(message.server.name), description="Here's what I could find.", color=0x00ff00)
-                                                    embed.set_author(name="SASRP")
+                                                    embed = discord.Embed(name="AG's info".format(message.server.name), description="Here's what I could find.", color=0x00ff00)
+                                                    embed.set_author(name="Adrenaline Gaming")
                                                     embed.add_field(name="Name", value=message.server.name, inline=True)
                                                     embed.add_field(name="ID", value=message.server.id, inline=True)
                                                     embed.add_field(name="Roles", value=len(message.server.roles), inline=True)
                                                     embed.add_field(name="Members", value=len(message.server.members))
                                                     await client.send_message(message.channel, embed=embed)
-    if message.content.upper().startswith('?LINKS'):
-                                                    await client.send_message(message.channel, "Website-http://www.sas-rp.com/ , CAD-http://cad.sas-rp.com/home")
     if message.content.upper().startswith('?CMDS'):
                                                   embed3 = (discord.Embed(description=None, colour=0x00ff00))
                                                   embed3.set_author(name="Server Commands")
-                                                  embed3.add_field(name="?INFORM", value="?LINKS", inline=True)
-                                                  embed3.add_field(name="-gives you server information", value="-gives you links to the website and the CAD", inline=True)
-                                                  embed3.add_field(name="?8BALL", value="?LOL", inline=True)
-                                                  embed3.add_field(name="-gives you a random message", value="Well...", inline=True)
+                                                  embed3.add_field(name="?INFORM", value="", inline=False)
+                                                  embed3.add_field(name="-gives you server information", value="", inline=False)
+                                                  embed3.add_field(name="?8BALL", value="?LOL", inline=False)
+                                                  embed3.add_field(name="-gives you a random message", value="Well...", inline=False)
                                                   await client.send_message(message.channel, embed=embed3)
     if message.content.upper().startswith('?ADMINCMDS'):
-        if "417059335634681856" in [role.id for role in message.author.roles]:
+        if "465702997515829268" in [role.id for role in message.author.roles]:
                                                                              embed3 = (discord.Embed(description=None, colour=0x00ff00))
                                                                              embed3.set_author(name="Server Admin Commands")
-                                                                             embed3.add_field(name="?NP(your announcement)", value="?MPS(ask for more players)", inline=True)
-                                                                             embed3.add_field(name="N/A", value="N/A", inline=True)
-                                                                             embed3.add_field(name="?WARN @(WARNED PERSON) For (your words)", value="N/A", inline=True)
+                                                                             embed3.add_field(name="?NP(your announcement)", value="", inline=False)
+                                                                             embed3.add_field(name="", value="", inline=False)
+                                                                             embed3.add_field(name="?WARN @(WARNED PERSON) For (your words)", value="", inline=False)
                                                                              await client.send_message(message.channel, embed=embed3)
         else:
             await client.send_message(message.channel, "You Do Not Have Permission")
@@ -82,9 +87,9 @@ async def on_message(message):
         userID = message.author.id
         await client.send_message(message.channel, ":ping_pong: pong!")
     if message.content.upper().startswith('?WARN'):
-        if "443478160340680725" in [role.id for role in message.author.roles]:
+        if "465702997515829268" in [role.id for role in message.author.roles]:
                                                                               args = message.content.split(" ")
-                                                                              chan = client.get_channel("443486152754724904")
+                                                                              chan = client.get_channel("466021905820942356")
                                                                               embed = (discord.Embed(description=None, colour=0x00ff00))
                                                                               embed.add_field(name="Someone Has Been Warned By %s" % (message.author), value="%s" % (" ".join(args[1:])), inline=False)
                                                                               embed2 = (discord.Embed(description=None, colour=0x00ff00))
